e7416e9cac8361578786d4a32c1c86cc00740db6
Bsingh24 <71533797+Bsingh24@users.noreply.github.com>
2022-03-30 21:08:27 -0700

    Update HandVolumeTracking.py

--- HandVolumeTracking.py
+++ HandVolumeTracking.py
@@ -50,26 +50,39 @@ while True:
     img = detector.findHands(img)
     lmList = detector.findPosition(img,draw=False)
     if len(lmList) != 0:
-        # print(lmList[4],lmList[8])
+        x1,y1 = lmList[4][1],lmList[4][2] #Thumb
+        x2,y2 = lmList[8][1],lmList[8][2] #Index Finger
+        x3,y3 = lmList[12][1],lmList[12][2] #Middle Finger
+        x4,y4 = lmList[16][1],lmList[16][2] #Ring Finger
+        x5,y5 = lmList[20][1],lmList[20][2] #Pinky Finger
 
-        x1,y1 = lmList[4][1],lmList[4][2]
-        x2,y2 = lmList[8][1],lmList[8][2]
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x3, y3), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x4, y4), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x5, y5), 15, (255, 0, 255), cv2.FILLED)
+
         xc,yc = (x1+x2)//2, (y1+y2)//2
 
+        print(y1)
+        print(y2)
 
-
-        cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FILLED)
-        cv2.circle(img,(x2,y2),15,(255,0,255),cv2.FILLED)
-        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3 )
-        cv2.circle(img,(xc,yc),15,(255,0,255),cv2.FILLED)
+        if y2 > y1:
+            pyautogui.hotkey('volumedown')
+        elif y3 > y1:
+            pyautogui.hotkey('volumeup')
+        elif y4 > y1:
+            pyautogui.hotkey('stop')
+        elif y5 > y1:
+            pyautogui.hotkey('playpause')
 
         length = math.hypot(x2-x1,y2-y1)
         print(length)
 
         #hand range 300 to 50
-        vol = np.interp(length,[50,300],[minVol,maxVol])
-        volBar = np.interp(length,[50,300],[400,150])
-        volume.SetMasterVolumeLevel(vol, None)
+        # vol = np.interp(length,[50,300],[minVol,maxVol])
+        # volBar = np.interp(length,[50,300],[400,150])
+        # volume.SetMasterVolumeLevel(vol, None)
 
         if length<50:
             cv2.circle(img, (xc, yc), 15, (0, 255, 0), cv2.FILLED)
