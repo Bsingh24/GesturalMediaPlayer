44ab7250ae29012791341a64957d04b165d5fd26
Sean L <71533878+Sean02@users.noreply.github.com>
2022-05-03 20:03:00 -0700

    Added latest version with play/pause/stop

--- HandVolumeTracking.py
+++ HandVolumeTracking.py
@@ -42,6 +42,9 @@ volBar = 400
 
 globTF = 0
 playpauseTF = 0
+shiftTF = 0
+xylock = 0
+muteTF = 0
 tempy1 = 0
 tempy2 = 0
 tempy3 = 0
@@ -59,22 +62,29 @@ while True:
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        palmx, palmy = lmList[0][1], lmList[0][1]
+        palmx, palmy = lmList[0][1], lmList[0][2]
         thumbx, thumby = lmList[4][1], lmList[4][2]  # Thumb
         index_x, index_y = lmList[8][1], lmList[8][2]  # Index Finger
         middle_x, middle_y = lmList[12][1], lmList[12][2]  # Middle Finger
         ring_x, ring_y = lmList[16][1], lmList[16][2]  # Ring Finger
         pinky_x, pinky_y = lmList[20][1], lmList[20][2]  # Pinky Finger
+        
+        thumbkx, thumbky = lmList[2][1], lmList[2][2]  # Thumb knuckle
+        index_kx, index_ky = lmList[6][1], lmList[6][2]  # Index Finger knuckle
+        middle_kx, middle_ky = lmList[10][1], lmList[10][2]  # Middle Finger knuckle
+        ring_kx, ring_ky = lmList[14][1], lmList[14][2]  # Ring Finger knuckle
+        pinky_kx, pinky_ky = lmList[18][1], lmList[18][2]  # Pinky Finger knuckle
 
-        cv2.circle(img, (thumbx, thumby), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (index_x, index_y), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (middle_x, middle_y), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (ring_x, ring_y), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (pinky_x, pinky_y), 15, (255, 0, 255), cv2.FILLED)
+
+        cv2.circle(img, (thumbx, thumby), 15, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (index_x, index_y), 15, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (middle_x, middle_y), 15, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (ring_x, ring_y), 15, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (pinky_x, pinky_y), 15, (255, 0, 0), cv2.FILLED)
 
         xc, yc = (thumbx + index_x) // 2, (thumby + index_y) // 2
 
-        if (index_y < thumby) and (middle_y < thumby) and (ring_y < thumby) and (pinky_y < thumby):
+        if (index_y < thumby) and (middle_y < thumby) and (ring_y < thumby) and (pinky_y < thumby) and index_y < index_ky and (middle_y < middle_ky) and (ring_y < ring_ky) and (pinky_y < pinky_ky):
             tempy1 = 0
             tempy2 = 0
             tempy3 = 0
@@ -87,6 +97,9 @@ while True:
             tempx5 = 0
             globTF = 0
             playpauseTF = 0
+            shiftTF = 0
+            xylock = 0
+            muteTF = 0
 
         # # these are the lower the level
         # if (index_y > thumby) and (middle_y < thumby) and (ring_y < thumby) and (pinky_y < thumby):  # index finder
@@ -137,6 +150,16 @@ while True:
         #             tempx4 = 0
         #             tempx5 = 0
         #
+
+
+        if (index_y < thumby) and (middle_y > thumby) and (ring_y > thumby) and (pinky_y > thumby):
+            if(playpauseTF == 0):
+                pyautogui.hotkey('playpause')
+                playpauseTF = 1
+
+        if (index_y < thumby) and (middle_y < thumby) and (ring_y > thumby) and (pinky_y > thumby):
+            pyautogui.hotkey('stop')
+
         # # skip videos (at least on youtube). Next is shift + n, and last is shift + p.
         # # hand symbol for this is all fingers down(aside from thumb). Then drag hand left to go back, and right to skip video. right now it is using short cuts...
         # # so might need to change it.
@@ -153,10 +176,11 @@ while True:
                 tempx4 = ring_x
                 tempx5 = pinky_x
                 globTF = 1
+                
             if (index_y > thumby) and (middle_y > thumby) and (ring_y > thumby) and (pinky_y > thumby):
                 # if (index_y < thumby) and (middle_y < thumby) and (ring_y < thumby):
                 # if (pinky_y > thumby) and (pinky_y < thumby + 10):
-                if (thumbx < tempx1 - 75):
+                if (thumbx < tempx1 - 75 and shiftTF == 0):
                     pyautogui.keyDown('shift')
                     pyautogui.press('p')
                     pyautogui.keyUp('shift')
@@ -171,7 +195,8 @@ while True:
                     tempx3 = 0
                     tempx4 = 0
                     tempx5 = 0
-                if (thumbx > tempx1 + 75):
+                    shiftTF = 1
+                if (thumbx > tempx1 + 75 and shiftTF == 0):
                     pyautogui.keyDown('shift')
                     pyautogui.press('n')
                     pyautogui.keyUp('shift')
@@ -186,6 +211,7 @@ while True:
                     tempx3 = 0
                     tempx4 = 0
                     tempx5 = 0
+                    shiftTF = 1
 
         
 
@@ -194,12 +220,14 @@ while True:
         #     pyautogui.hotkey('volumemute')
         lengthfist = math.hypot(index_x - thumbx, index_y - thumby)
         # print(lengthfist)
-        # if (lengthfist < 50):
-        #     x = time.perf_counter()
-        #     print("X", x)
-        #     if (int(x - t) == 3):
-        #         pyautogui.hotkey('volumemute')
-        #         t = time.perf_counter()
+        if (lengthfist < 50 and muteTF == 0):
+            # x = time.perf_counter()
+            # print("X", x)
+            # if (int(x - t) == 3):
+            muteTF = 1
+            pyautogui.hotkey('volumemute')
+            t = time.perf_counter()
+
         # if (middle_y > index_y):
         #     pyautogui.hotkey('volumeup')
         # if (index_y > pinky_y and lengthfist > 50):
@@ -207,10 +235,10 @@ while True:
 
         # if all tips are on the same x-axis, fast-forward or rewind mode
         # move flat hand left for rewind, right for fast forward
-        xylock = 0
+        
         if (index_x <= thumbx + 5 or index_x >= thumbx - 5) and (middle_x <= thumbx + 5 or middle_x >= thumbx - 5) and (
                 ring_x <= thumbx + 5 or ring_x >= thumbx - 5) and (
-                pinky_x <= thumbx + 5 or pinky_x >= thumbx - 5) and xylock == 0 and (index_y < thumby) and (middle_y < thumby) and (ring_y < thumby):
+                pinky_x <= thumbx + 5 or pinky_x >= thumbx - 5)  and (index_y < thumby) and (middle_y < thumby) and (ring_y < thumby) and (index_y < pinky_y):
             # cv2.putText(img, "FFW or RWD", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
 
             if (globTF == 0):
@@ -225,7 +253,7 @@ while True:
                 tempx4 = ring_x
                 tempx5 = pinky_x
                 globTF = 1
-                xylock = 1
+
             if (index_x <= thumbx + 5 or index_x >= thumbx - 5) and (
                     middle_x <= thumbx + 5 or middle_x >= thumbx - 5) and (
                     ring_x <= thumbx + 5 or ring_x >= thumbx - 5) and (pinky_x <= thumbx + 5 or pinky_x >= thumbx - 5):
@@ -245,6 +273,7 @@ while True:
                     tempx4 = 0
                     tempx5 = 0
                     xylock = 0
+                    
                 if (thumbx < tempx1 - 50):
                     # cv2.putText(img, "RWD", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
                     # pyautogui.press('left')
@@ -264,8 +293,8 @@ while True:
         # if all tips are on the same y-axis, we increase or decrease the volume
         # move flat hand left for rewind, right for fast forward
         if (index_y <= thumby + 5 or index_y >= thumby - 5) and (middle_y <= thumby + 5 or middle_y >= thumby - 5) and (
-                ring_y <= thumby + 5 or ring_y >= thumby - 5) and (
-                pinky_y <= thumby + 5 or pinky_y >= thumby - 5) and xylock == 0:
+                ring_y <= thumby + 5 or ring_y >= thumby - 5) and (pinky_y > thumby) and (pinky_y > middle_y) and (pinky_y > ring_y) and xylock == 0: 
+                                                                    #+ 5 or pinky_y >= thumby - 5) and xylock == 0:
             # cv2.putText(img, "FFW or RWD", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
 
             if (globTF == 0):
@@ -284,8 +313,9 @@ while True:
             if (index_y <= thumby + 3 or index_y >= thumby - 3) and (
                     middle_y <= thumby + 3 or middle_y >= thumby - 3) and (
                     ring_y <= thumby + 3 or ring_y >= thumby - 3) and (pinky_y <= thumby + 3 or pinky_y >= thumby - 3):
-                if (thumby > tempy1 + 10):
+                if (thumby > tempy1 + 20):
                     # cv2.putText(img, "RWD", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
+                    pyautogui.hotkey('volumedown')
                     pyautogui.hotkey('volumedown')
                     globTF = 0
                     tempy1 = 0
@@ -299,9 +329,10 @@ while True:
                     tempx4 = 0
                     tempx5 = 0
                     xylock = 0
-                if (thumby < tempy1 - 10):
+                if (thumby < tempy1 - 20):
                     # cv2.putText(img, "RWD", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
                     # pyautogui.press('left')
+                    pyautogui.hotkey('volumeup')
                     pyautogui.hotkey('volumeup')
                     globTF = 0
                     tempy1 = 0
@@ -315,40 +346,12 @@ while True:
                     tempx4 = 0
                     tempx5 = 0
                     xylock = 0
-        
-        
-        
-        lengthfist = math.hypot(index_x - thumbx, index_y - thumby)
-        if(lengthfist < 50):
-            if (globTF == 0):
-                tempy1 = thumby
-                tempy2 = index_y
-                tempy3 = middle_y
-                tempy4 = ring_y
-                tempy5 = pinky_y
-                tempx1 = thumbx
-                tempx2 = index_x
-                tempx3 = middle_x
-                tempx4 = ring_x
-                tempx5 = pinky_x
-                globTF = 1
-        
-            if math.hypot(tempx2 - tempx1, tempy2 - tempy1) < 50:
-                pyautogui.hotkey('volumemute')
-                globTF = 0
-                tempy1 = 0
-                tempy2 = 0
-                tempy3 = 0
-                tempy4 = 0
-                tempy5 = 0
-                tempx1 = 0
-                tempx2 = 0
-                tempx3 = 0
-                tempx4 = 0
-                tempx5 = 0
-                xylock = 0
-                
-                
+        # hand range 300 to 50
+        # vol = np.interp(length,[50,300],[minVol,maxVol])
+        # volBar = np.interp(length,[50,300],[400,150])
+        # volume.SetMasterVolumeLevel(vol, None)
+
+        if lengthfist < 50:
             cv2.circle(img, (xc, yc), 15, (0, 255, 0), cv2.FILLED)
 
 
